297583b73dd58ee83d2ef0c2552ae53e2b1feb7a
Gardner Bickford <gardner@bickford.nz>
2021-01-12 17:57:37 +1300

    🌅  Initial commit  🌅

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `SV Grey Spirit`,
+    author: `Gardner Bickford`,
+    description: `Stories from the sea`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/greyspiritnz`,
+      },
+    ],
+  },
+}
